a679b20645494dadb5018ec51511e1a2abdaac6d
cpea2506 <cpea2506@users.noreply.github.com>
2022-12-12 20:07:19 +0000

    deploy: cafbb03c518391133a39217ce2fa588d0f0ab077

--- flappybust-12bc37e1289b2645.d.ts
+++ flappybust-12bc37e1289b2645.d.ts
@@ -56,12 +56,10 @@ export interface InitOutput {
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly __wbindgen_export_3: WebAssembly.Table;
   readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1812211d633f8476: (a: number, b: number) => void;
-  readonly closure1389_externref_shim: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1a9abb5e964c86e0: (a: number, b: number, c: number) => void;
   readonly __wbindgen_free: (a: number, b: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
-  readonly __externref_table_alloc: () => number;
   readonly __wbindgen_start: () => void;
 }
 
