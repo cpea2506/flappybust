e533c4ed8c20ab73ca93610488b6f244f36ce599
CPea <vienpro2506@gmail.com>
2023-02-01 02:13:26 +0700

    feat: moving background

--- src/game/background.rs
+++ src/game/background.rs
@@ -1,24 +1,67 @@
-use super::{DateTime, GameState};
+use crate::{constants::SCREEN_WIDTH, GameState};
 use bevy::prelude::*;
-use iyes_loopless::prelude::AppLooplessStateExt;
+use iyes_loopless::prelude::{AppLooplessStateExt, IntoConditionalSystem};
+
+use super::datetime::DateTime;
 
 #[derive(Component, Default, Clone, Copy)]
-struct Background;
+struct Background {
+    translation: Vec3,
+    secondary: bool,
+}
+
+impl Background {
+    fn new(x: f32, y: f32, secondary: bool) -> Self {
+        Background {
+            translation: Vec3 { x, y, z: 0. },
+            secondary,
+        }
+    }
+
+    fn generate_bundle(self, texture: &Handle<Image>) -> (SpriteBundle, Self) {
+        (
+            SpriteBundle {
+                texture: texture.clone(),
+                transform: Transform::from_translation(self.translation),
+                ..default()
+            },
+            self,
+        )
+    }
+}
 
 pub struct BackgroundPlugin;
 
 impl Plugin for BackgroundPlugin {
     fn build(&self, app: &mut App) {
-        app.add_enter_system(GameState::Ready, spawn);
+        app.add_enter_system(GameState::Ready, spawn)
+            .add_system(moving.run_in_state(GameState::Playing));
     }
 }
 
 fn spawn(mut commands: Commands, asset_server: Res<AssetServer>, datetime: Res<DateTime>) {
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load(format!("images/bg_{}.png", *datetime)),
-            ..default()
-        },
-        Background,
-    ));
+    let background = Background::default();
+    let secondary_background =
+        Background::new(background.translation.x, background.translation.y, true);
+    let texture = asset_server.load(&format!("images/bg_{}.png", *datetime));
+
+    commands.spawn_batch(vec![
+        background.generate_bundle(&texture),
+        secondary_background.generate_bundle(&texture),
+    ]);
+}
+
+fn moving(mut background: Query<(&mut Background, &mut Transform)>) {
+    let screen_width = SCREEN_WIDTH;
+
+    for (mut background, mut transform) in &mut background {
+        background.translation.x = (background.translation.x - 1.) % screen_width;
+
+        transform.translation.x = background.translation.x
+            + if background.secondary {
+                SCREEN_WIDTH
+            } else {
+                0.
+            };
+    }
 }
